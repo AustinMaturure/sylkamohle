7e0bd4856417c7e28d86f5c0929472eb488a5dc6
Austin <austinematurure@gmail.com>
2024-01-18 07:11:24 -0800

    updatedviteconfig

--- sylkemohle/vite.config.js
+++ sylkemohle/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    external: ['react-intersection-observer']
-  }
+  build: {
+    rollupOptions: {
+      external: ['react-intersection-observer'],
+    },
+  },
 })

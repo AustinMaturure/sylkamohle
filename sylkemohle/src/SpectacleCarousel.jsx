471b967833db9b8c3eeda551551305df63c51e86
Austin <austinematurure@gmail.com>
2024-01-20 07:15:35 -0800

    addedanimations

--- sylkemohle/src/SpectacleCarousel.jsx
+++ sylkemohle/src/SpectacleCarousel.jsx
@@ -5,26 +5,39 @@ import oakleyLogo from '../assets/Oakley.svg';
 import michaelkorsLogo from '../assets/Michael Kors.svg';
 import polaroidLogo from '../assets/Polaroid.svg';
 import tomFordLogo from '../assets/Tom Ford.svg';
+import guessLogo from '../assets/Guess_logo.svg';
+import pumaLogo from '../assets/puma.svg';
 import eye from '../assets/eye-2.jpg';
 import eyeSun from '../assets/eye-sunglasses.jpg';
 import './Hero.css';
+import useElementInView from './ElementInView';
 
-const logos = [raybanLogo, bossLogo, oakleyLogo, michaelkorsLogo, polaroidLogo, tomFordLogo];
+const logos = [raybanLogo, bossLogo, oakleyLogo, michaelkorsLogo, polaroidLogo, tomFordLogo,,guessLogo, pumaLogo ];
 
 const SpectacleCarousel = () => {
   const [eyeImage, setEyeImage] = useState(eye);
 
   const handleClick = () => {
     setEyeImage(eyeImage === eye ? eyeSun : eye);
+    const log=document.querySelectorAll('.Logo-img')
+    log.forEach((image, index) => {
+       image.style.filter= 'brightness(0) invert(1)' 
+
+      });
 
     
   };
 
+  const persHRef = useElementInView('.pers-header');
+  const persDRef = useElementInView('.pers-desc');
+  const presHRef = useElementInView('#pers-head-disc');
+  const presDRef = useElementInView('#pers-desc-disc')
+
   return (
     <>
       
-      <h1 className='pers-header'>Need A Prescription?</h1>
-      <h3 className='pers-desc'>No matter the Budget, Choose from a range of Popular Brands </h3>
+      <h1 className='pers-header' ref={persHRef}>Need A Prescription?</h1>
+      <h3 className='pers-desc' ref={persDRef}>No matter the Budget, Choose from a range of Popular Brands </h3>
       <div className="pres-sun">
         <h1 className='pers-header' id='pers-head-sun'>Need Sunglasses?</h1>
         <h3 className='pers-desc' id='pers-desc-sun'>Turn Any Prescription frame into Sunglasses... no seriously    <button className='try-it-btn' onClick={handleClick}>TRY IT</button></h3>
@@ -45,8 +58,8 @@ const SpectacleCarousel = () => {
         </div>
       </div>
       <div className="pres-disc">
-        <h1 className='pers-header' id='pers-head-disc'>Need A Discount?</h1>
-        <h3 className='pers-desc' id='pers-desc-disc'>Are you a Pensioner? Grab a eye test for only R400, and reading glasses from as low as R1000
+        <h1 className='pers-header' id='pers-head-disc' ref={presHRef}>Need A Discount? </h1>
+        <h3 className='pers-desc' id='pers-desc-disc' ref={presDRef}>Are you a Pensioner? Grab a eye test for only R400, and reading glasses from as low as R1000
        </h3>
       </div>
     </>

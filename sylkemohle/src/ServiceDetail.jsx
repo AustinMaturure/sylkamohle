29b668dfd3cd7cdaff9631a045008292f9d544b9
Austin <austinematurure@gmail.com>
2024-01-29 12:30:10 -0800

    added OCt image, updated Text and added logic to decide which image is displayed

--- sylkemohle/src/ServiceDetail.jsx
+++ sylkemohle/src/ServiceDetail.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import '../src/servicedetail.css'
 import './Hero.css'
 import spec from '../assets/pexels-francesco-paggiaro-1627639.jpg'
+import oct from '../assets/oct.jpg'
 import serviceData from '../src/servicesData.json'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -33,12 +34,12 @@ export default function ServiceDetail() {
           </ul>
           
           
-          <button className="book-btn">BOOK</button>
+          <button className="book-btn">CALL US</button>
         </nav>
       </div> {console.log(serviceData.services)}
             <div className="service-detail-cnt">
                 <div className="service-illustration">
-                            <img src={spec} alt="" className="serv-pic" />
+                            <img src={serviceId == 4 ? spec : oct} alt="perscription-eyeglasses-piet-retief" className="serv-pic" loading="lazy"/>
 
                         </div>
                         <div className="service-descrption" key={service.id}>

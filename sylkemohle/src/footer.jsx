3657cb45da067f617e33cf614f92c1627b739406
Austin <austinematurure@gmail.com>
2024-02-26 23:54:47 -0800

    performance updates

--- sylkemohle/src/footer.jsx
+++ sylkemohle/src/footer.jsx
@@ -27,7 +27,7 @@ export default function Footer(){
   ></iframe>
 </div>
         <div className="foot-contact">
-          <h3 className='call-whatsapp'> <FontAwesomeIcon icon={faMessage} className="clock-icon"/>(+27) 82 554 2925</h3>
+          <h3 className='call-whatsapp'> <FontAwesomeIcon icon={faPhone} className="clock-icon"/>(+27) 82 554 2925</h3>
         <h3 className='foot-call-contact'> <FontAwesomeIcon icon={faEnvelope} className="clock-icon"/>sylkamohle@intekom.co.za</h3>
         <h3>
    
